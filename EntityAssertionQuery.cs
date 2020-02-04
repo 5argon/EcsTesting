e7b26a96a99cc6a41acfff8c9661dfdd1f072474
5argon <pyasry@gmail.com>
2020-02-04 10:43:52 +0700

    Add some experimental methods for component objects.

--- EntityAssertionQuery.cs
+++ EntityAssertionQuery.cs
@@ -1,4 +1,7 @@
-﻿using Unity.Entities;
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
 
 namespace E7.EcsTesting
 {
@@ -14,9 +17,72 @@ namespace E7.EcsTesting
     public partial class EntityAssertionQuery
     {
         EntityManager em;
+
         public EntityAssertionQuery(World world)
         {
             this.em = world.EntityManager;
+        }
+
+        public int EntityCountComponentObject<CD>(Func<CD, bool> where = null) where CD : class, IComponentData
+        {
+            return ComponentObjects<CD>(where).Length;
+        }
+
+        public CD[] ComponentObjects<CD>(Func<CD, bool> where = null) where CD : class, IComponentData
+        {
+            CD[] array;
+            using (var eq = em.CreateEntityQuery(
+                ComponentType.ReadOnly<CD>()
+            ))
+            {
+                var na = eq.ToEntityArray(Allocator.TempJob);
+                array = new CD[na.Length];
+                for (int i = 0; i < na.Length; i++)
+                {
+                    array[i] = em.GetComponentObject<CD>(na[i]);
+                }
+            }
+
+            if (where == null)
+            {
+                return array;
+            }
+
+            List<CD> list = new List<CD>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (where.Invoke(array[i]) == true)
+                {
+                    list.Add(array[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        public CD GetSingleObject<CD>() where CD : class, IComponentData
+        {
+            return em.GetComponentObject<CD>(GetSingleEntityObject<CD>());
+        }
+
+        public Entity GetSingleEntityObject<CD>() where CD : class, IComponentData
+        {
+            using (var eq = em.CreateEntityQuery(
+                ComponentType.ReadOnly<CD>()
+            ))
+            {
+                var na = eq.ToEntityArray(Allocator.TempJob);
+
+                var array = na.ToArray();
+                na.Dispose();
+                if (array.Length != 1)
+                {
+                    throw new System.InvalidOperationException(
+                        $"GetSingleObject() requires that exactly one exists but there are {array.Length}.");
+                }
+
+                return array[0];
+            }
         }
     }
 }
